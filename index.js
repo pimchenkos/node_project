84e648a7bfc83a1d2b6c4544f6c862552969db69
Pimchenko Sergey <spimchenko@pari.ru>
2024-04-23 14:38:18 +0300

    module export

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-let x = 5;
-let y = 4;
-const num = x*y;
-console.log(`Variable: ${num}`)
+const os = require('os')
+let res= os.platform()
+console.log(res)
+
+const my_math = require('./my_math.js')
+let res1 = my_math.add(4, 5)
+let res2 = my_math.minus(7, 5)
+
+console.log(`Res1: ${res1}, Res2: ${res2}`)
